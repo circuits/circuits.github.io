7ed342324f0cbc6a33c87e9cbd737d567af3e132
James Mills <prologic@shortcircuit.net.au>
2015-06-30 19:00:48 +1000

    Update published SITEURL

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *  # noqa
 
-SITEURL = "https://circuits.github.io"
+SITEURL = "https://circuitsframework.com"
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = "feeds/all.atom.xml"
